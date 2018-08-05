ae176deee43be16a2f6fd1788eb581183e3db5cb
MonsakOB <40932781+MonsakOB@users.noreply.github.com>
2018-08-05 11:07:41 +0700

    Create bot_settings.php

---
+++ bot_settings.php
@@ -0,0 +1,7 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('1593011483','กรอก ค่า Channel ID');
+define('55609e9ab0f8bcc68afc9ac038f49db8','กรอกค่า Channel secret');
+define('ZERwG1f0dxKME/RYFhBVwoYatwuiADOgkiqN8Eh4CixfXUxi/YB2cPJcGCUCyHdIMRYroHjqfoXdaki9gQ2Vf4ddeEWTlNtywhdeYgTmzHQyzNzjVZ94vjekkelPEjBvWOD+lH9So9wVr1q+BrCzFwdB04t89/1O/w1cDnyilFU=
+','กรอกค่า Channel access token');
+?>
